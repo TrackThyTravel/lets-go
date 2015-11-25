2d5e7a12d7a2c50e7b5ea8e6b06bce7b13bf8b68
Nithin Chakravarthy <nithinchakravarthy@nithins-macbook-pro.local>
2015-11-25 11:56:30 -0800

    Day 1

--- TrackThyTravel/ViewController.swift
+++ TrackThyTravel/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("View Did Load")
+        print("View Did Load  ")
     }
 
     override func didReceiveMemoryWarning() {
