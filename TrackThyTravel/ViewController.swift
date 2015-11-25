927bf92cbfd44a047ba8963c2c20f0bec305edbd
Nithin Chakravarthy <nithinchakravarthy@nithins-macbook-pro.local>
2015-11-25 11:28:24 -0800

    Day 1

--- TrackThyTravel/ViewController.swift
+++ TrackThyTravel/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("View Did Load")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
