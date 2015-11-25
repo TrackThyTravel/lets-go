5aac5c1b3147530160e6c081c158dcb8b5302477
shrutibhandakkar <shruti.bhandakkar@gmail.com>
2015-11-25 12:01:33 -0800

    third commit

--- TrackThyTravel/ViewController.swift
+++ TrackThyTravel/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("View Did Load  ")
         print("second commit ")
+         print("second commit !!")
+        
     }
 
     override func didReceiveMemoryWarning() {
